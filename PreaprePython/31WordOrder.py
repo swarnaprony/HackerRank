c13ce18fba3e769b00c304c508441bed691248bb
swarnaprony <swarnaprony81@gmail.com>
2022-12-08 20:48:00 +0100

    Add mergeTools and WordOrder.py

---
+++ PreaprePython/31WordOrder.py
@@ -0,0 +1,30 @@
+
+def order_words(words):
+    word_list = {}
+    word_number = []
+    distinct_word = 0
+    splited_word = words
+    for word in splited_word:
+        if word in word_list.keys():
+            word_list[word].append(word)
+        else:
+            distinct_word += 1
+            word_list[word] = [word]
+    print(distinct_word)
+
+    for i in word_list.keys():
+        word_number.append(len(word_list.get(i)))
+    print(*word_number, sep=' ')
+
+
+
+if __name__ == '__main__':
+    number = int(input())
+    i = 0
+    words = []
+    while i < number:
+        word = input()
+        words.append(word)
+        i = i+1
+    order_words(words)
+
